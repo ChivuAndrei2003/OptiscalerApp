0ad4446c187ca3121ad75cf5f48a5730eaf7cfd2
Chivu Andrei <chivuandrei2003@gmail.com>
2026-07-03 22:23:49 +0300

    update main page

--- OptiscalerApp/Views/GamesView.axaml.cs
+++ OptiscalerApp/Views/GamesView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace OptiscalerApp.Views;
@@ -9,5 +10,10 @@ public partial class GamesView : UserControl
     public GamesView()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
