3f284e035000324dfceb07c6356e1a0332467f8b
Chivu Andrei <chivuandrei2003@gmail.com>
2026-09-02 14:30:49 +0300

    add the contract forr the app's paths

--- Optiscaler.Core/Abstractions/IAppPaths.cs
+++ Optiscaler.Core/Abstractions/IAppPaths.cs
@@ -2,5 +2,17 @@ namespace Optiscaler.Core.Abstractions;
 
 public interface IAppPaths
 {
+    string RootDirectory { get; }
+
+    string GamesFilePath { get; }
+    string ConfigurationFilePath { get; }
+
+    string AnalysisCacheFilePath { get; }
+
+    string ArtifactCacheDirectory { get; }
+    
+    string ProfilesDirectory { get; }
+    
+    string TransactionsDirectory { get; }
     
 }
